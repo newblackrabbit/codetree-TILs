4c36d890efb4383db0c7f9b5c9e4c7390fbbd089
YunHyeong <150113466+newblackrabbit@users.noreply.github.com>
2024-08-05 11:45:00 +0900

    Title: 특정 조건에 맞게 출력하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240805/특정 조건에 맞게 출력하기/print-in-specific-conditions.py
@@ -0,0 +1,14 @@
+lst = list(map(int,input().split()))
+
+for i in range(len(lst)):
+    if lst[i] == 0:
+        lst = lst[:i]
+
+for i in range(len(lst)):
+    if lst[i] % 2 == 0:
+        lst[i] = lst[i] // 2
+    else:
+        lst[i] = lst[i] + 3
+
+for i in lst:
+    print(i,end=' ')
